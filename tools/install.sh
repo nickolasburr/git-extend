f637b10ab0ff72c1d50a6f0f7bca0617e77717fb
Nickolas Burr <nickolasburr@gmail.com>
2018-03-04 15:28:26 -0500

    Refactor build.sh

--- tools/install.sh
+++ tools/install.sh
@@ -4,10 +4,26 @@ set -ex
 
 PREFIX="$1"
 
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+RESET="\033[0m"
+
 if [[ ! -d "$PREFIX" ]]; then
-	echo "$PREFIX not found, creating..."
+	echo -e "${YELLOW}$PREFIX does not exist, attempting to create it...${RESET}"
+
+	# Parent directory of $PREFIX.
+	DIRNAME="$(dirname $PREFIX)"
+
+	if [[ ! -w $DIRNAME ]]; then
+		echo -e "${RED}Cannot create $PREFIX, $DIRNAME is not writable. Make $DIRNAME writable or install elsewhere. Exiting...${RESET}"
+
+		exit 1
+	fi
 
 	mkdir -p "$PREFIX"
+
+	echo -e "${GREEN}Successfully created ${PREFIX}${RESET}"
 fi
 
 GIT="git"
@@ -18,11 +34,11 @@ BINDIR="bin"
 ETCDIR="etc"
 SRCDIR="src"
 
-[[ ! -d "$PREFIX/$BINDIR" ]] && mkdir -p "$PREFIX/$BINDIR"
-[[ ! -d "$PREFIX/$ETCDIR" ]] && mkdir -p "$PREFIX/$ETCDIR"
+[[ ! -d "$PREFIX/$BINDIR" ]] && mkdir -p "$PREFIX/$BINDIR" && echo -e "${GREEN}Successfully created ${PREFIX}/${BINDIR}${RESET}"
+[[ ! -d "$PREFIX/$ETCDIR" ]] && mkdir -p "$PREFIX/$ETCDIR" && echo -e "${GREEN}Successfully created ${PREFIX}/${ETCDIR}${RESET}"
 
-if [[ -f "$PREFIX/$BINDIR/$GIT" ]]; then
-	echo "Found an executable at $PREFIX/$BINDIR/$GIT. Exiting..."
+if [[ -f "$PREFIX/$BINDIR/$GIT" ]] && [[ ! "$PREFIX/$BINDIR/$GIT" -ef "$PREFIX/$BINDIR/$TARGET" ]]; then
+	echo -e "${RED}Found an executable at $PREFIX/$BINDIR/$GIT. Remove it manually to install here. Exiting...${RESET}"
 
 	exit 1
 fi
