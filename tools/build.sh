90416ed8060eec761894a9c4f20068035ed50195
Nickolas Burr <nickolasburr@gmail.com>
2018-03-10 16:15:55 -0500

    Reapply 590beb1 to build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -5,14 +5,15 @@ set -ex
 GITPREFIX="$1"
 
 if [[ ! -d "$GITPREFIX" ]]; then
-	echo "$GITPREFIX is not a valid directory."
+	echo "$GITPREFIX is not a valid prefix. Exiting..."
 
 	exit 1
 fi
 
 TARGET="git-extend"
 BINDIR="bin"
-DEFDIR="/usr/local"
+# Default GITPREFIX value in git-extend.
+DEFDIR="/usr"
 
 SED="/usr/bin/sed"
 SEDOPTS="-i ''"
@@ -20,5 +21,5 @@ SEDMATCH="s@$DEFDIR@$GITPREFIX@g"
 
 cd ..
 
-# Update PREFIX in git-extend, if needed.
+# Update GITPREFIX in git-extend, if needed.
 [[ $GITPREFIX != $DEFDIR ]] && eval "$SED $SEDOPTS $SEDMATCH $BINDIR/$TARGET"
